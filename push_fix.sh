111918a391ea575dc86b8a397e7d8a7ef958c967
mrosale2 <mattrosa@umich.edu>
2026-04-27 18:03:55 -0500

    Mobile responsive + McKinsey design overhaul

--- push_fix.sh
+++ push_fix.sh
@@ -2,7 +2,7 @@
 cd ~/Documents/Matt_Madden/XXVIAbilities
 rm -f .git/index.lock
 git add -A
-git commit -m "Fix all filters, labels, merged tabs, position audit"
+git commit -m "Mobile responsive + McKinsey design overhaul"
 git push -f origin main
 echo ""
 echo "=== DONE! Push complete. Press any key to close. ==="
